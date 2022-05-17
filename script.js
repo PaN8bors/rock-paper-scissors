dba83f58d56d0d9ecc3dbdc444681d86829be4cf
Parish Neighbors <pn8bors@gmail.com>
2022-05-17 09:06:45 -0500

    Eighth commit.  Add reset button.

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ let scoreBoard = document.getElementById("yourScore");
 let scoreValue = sessionStorage.getItem("score");
 
 // Check for browser refresh
-// JSON.parse(scoreValue) == null ? curScore = 0 : scoreBoard.innerHTML = curScore = scoreValue;
+// scoreValue == null ? curScore = 0 : scoreBoard.innerHTML = curScore = scoreValue;
 scoreBoard.innerHTML = curScore = scoreValue || 0;
 console.log(curScore);
+
+function resetGame() {
+  curScore = 0;
+  sessionStorage.setItem("score", 0);
+  document.getElementById("results").innerHTML = "RESULTS";
+  document.getElementById("yourScore").innerHTML = "SCORE";
+  document.getElementById("playerChoice").innerHTML = "You -";
+  document.getElementById("houseChoice").innerHTML = "House -";
+  document.getElementById("yoursImg").src = "./assets/mickeyR.png";
+  document.getElementById("housesImg").src = "./assets/mickeyL.png";
+}
 
 // Keep score
 function saveScore() {
